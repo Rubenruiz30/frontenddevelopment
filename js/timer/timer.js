743745e6d123c74492cc9b4e6d18df39879da875
Ruben <davidruizglobal30@gmail.com>
2026-02-11 11:33:18 +0100

    ejercicio timer 11/02/26

---
+++ js/timer/timer.js
@@ -0,0 +1,12 @@
+
+/* "document* object represents the whole html document */
+//a tree data structure
+// document.getElementById() method returns the element that has the ID attribute with the specified value. If no such element exists, it returns null.
+//this method or command allows us to retrieve an elemnt
+// in our page its id
+const startButton = document.getElementById('startButton');
+const stopButton = document.getElementById('stopButton');
+const resetButton = document.getElementById('resetButton');
+const timerLabel = document.getElementById('timerLabel');
+
+console.debug(timerLabel);
